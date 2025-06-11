091440a8e992e42fa3ebd8ed985f782614a884ea
Mariana-1324 <cindarojas2007@cesanrafael.org>
2025-06-11 14:13:41 -0400

    creamos archivo requieremenst

--- config/conexion.py
+++ config/conexion.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 conexion = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database="bdventas",
+    host="bqpwcgnzojllikvaaviz-mysql.services.clever-cloud.com",
+    user="u8ygdtfrdso7mprj",
+    password="02PXZqBPlbfaouN1YCfI",
+    database="bqpwcgnzojllikvaaviz",
     use_pure=True
 )
 if conexion.is_connected():
